dcfba80564414b2f45fb3e5381be6f6bcba0c781
Boris Sachet <boris.sachet@gmail.com>
2020-12-15 00:05:54 +0100

    Added printTabElastic methods to display two dimensions arrays of string with different column sizes.

--- src/Tools/Print.java
+++ src/Tools/Print.java
@@ -1,3 +1,5 @@
+package Tools;
+
 /**
  * This is a convenient toolbox for printing stuff
  *
@@ -216,6 +218,67 @@ public class Print {
         System.out.println();
     }
 
-    // TODO : two dimention printer for all other vars formats
+    /**
+     * Display two dimensional table of String
+     * Each column will be the size of the largest item in this column
+     * @param tab : table 2D of int
+     */
+    public static void printTabElastic(String[][] tab){
+        printTabElastic(tab,' ');
+    }
+
+    /**
+     * Display two dimensional table of String
+     * Each column will be the size of the largest item in this column
+     * @param tab table 2D of int
+     * @param border character used for borders between cells
+     */
+    // TODO report the various improvements made in this printer into the others
+    // Note tab.length gives the number of lines and tab[0].length give the number of columns
+    public static void printTabElastic(String[][] tab,Character border) {
+        int lineCount = 0, columnCount;
+        Integer[] columnSizes = new Integer[tab[0].length];
+
+        /*Initialize column size with the max size of the biggest line indicator*/
+        for (int i = 0; i<tab[0].length;i++) {columnSizes[i] = Get.getLength(tab[0].length);}
+
+        /* Run through the whole table and get the length of the biggest element in the table (to format nicely on display) */
+        for (String[] line : tab) {
+            columnCount = 0;
+            for (String columnCell : line) {
+                if (columnCell != null && columnCell.length() > columnSizes[columnCount]) {columnSizes[columnCount] = columnCell.length();}
+                columnCount++;
+            }
+        }
+
+        /* Display the first line with the columns numbers */
+        // Adaptable spacer to match column indicator length +2 for the ':' character and the table border '|'
+        System.out.print(Format.normaliseLength("", (Get.getLength(tab.length) + 1)) + border);
+        // Display column number for each column
+        for (int i = 0; i < tab[0].length; i++) {
+            // +border to account for space between columns
+            System.out.print(Format.normaliseLengthCenter(i,(columnSizes[i])) + border);
+        }
+        System.out.println();
+
+        /* Display of the content of the table */
+        // For each line in the table
+        for (String[] line : tab) {
+            // Display line number on the side, normalised to the biggest it can get
+            System.out.print(Format.normaliseLength(lineCount, Get.getLength(tab.length)) + ":" + border);
+            // For each column/item in this line // Note: this for can be replaced with the printTab method //TODO: DO JUST THAT
+            columnCount = 0;
+            for (String columnCell : line) {
+                // Display the cell content (length normalised and with border)
+                System.out.print(Format.normaliseLength(columnCell,columnSizes[columnCount]) + border);
+                columnCount++;
+            }
+            lineCount++;
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    // TODO : two dimension printer for all other vars formats
     
 }
